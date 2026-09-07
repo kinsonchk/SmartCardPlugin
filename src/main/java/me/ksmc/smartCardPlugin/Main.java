853885ae5ef63ca229c78f405d2f3de7430766b9
kinsonchk <95203053+kinsonchk@users.noreply.github.com>
2026-09-07 13:33:32 -0400

    Basic plugin setup

--- src/main/java/me/ksmc/smartCardPlugin/Main.java
+++ src/main/java/me/ksmc/smartCardPlugin/Main.java
@@ -7,10 +7,12 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Plugin has been enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Plugin has been disabled!");
     }
 }
